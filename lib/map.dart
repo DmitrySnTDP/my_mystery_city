a53453d481d4ce20a0443c547f1d03f0075e624f
Magamid-Inc <magamid_inc@yahoo.com>
2025-04-17 01:21:29 +0500

    1.21 17.04.2025

--- lib/map.dart
+++ lib/map.dart
@@ -14,7 +14,6 @@ class _MapPageState extends State<MapPage> {
   @override
   // ignore: unused_field, override_on_non_overriding_member
   MapWindow? _mapWindow;
-
   
   @override
   
@@ -25,6 +24,9 @@ class _MapPageState extends State<MapPage> {
           onMapCreated: (mapWindow)
           {
             _mapWindow = mapWindow;
+            mapWindow.map.move(
+              CameraPosition(Point(latitude: 56.8519, longitude: 60.6122), zoom: 12.5, azimuth: 150.0, tilt: 30.0)
+            );
             mapkit.onStart();
           }
         )
